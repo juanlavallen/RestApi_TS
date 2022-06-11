127ee46aceb8932c6db75df861ffd2a67bd2871c
Juan Lavallen <juanlavallen02@gmail.com>
2022-06-11 16:35:02 -0300

    getUsers added

--- src/user/controllers/user.controller.ts
+++ src/user/controllers/user.controller.ts
@@ -3,4 +3,13 @@ import { UserService } from '../services/user.service';
 
 export class UserController {
   constructor(private readonly userService: UserService = new UserService()) {}
+
+  async getUsers(req: Request, res: Response) {
+    try {
+      const data = await this.userService.findAllUsers();
+      res.status(200).json(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
