d69726339b9ef3ed8a19e422b88468c5162baa8b
Juan Lavallen <juanlavallen02@gmail.com>
2022-06-19 13:16:12 -0300

    getCategories controller added

--- src/category/controllers/category.controller.ts
+++ src/category/controllers/category.controller.ts
@@ -3,4 +3,13 @@ import { CategoryService } from '../services/category.service';
 
 export class CategoryController {
   constructor(private readonly categoryService: CategoryService) {}
+
+  async getCategories(req: Request, res: Response) {
+    try {
+      const data = await this.categoryService.findAllCategories();
+      res.status(200).json(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 }
