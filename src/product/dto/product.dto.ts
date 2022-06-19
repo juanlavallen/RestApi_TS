df88e5b76ecb73b66cc5ab23f940ff402ee38398
Juan Lavallen <juanlavallen02@gmail.com>
2022-06-19 17:12:13 -0300

    Product Dto added

---
+++ src/product/dto/product.dto.ts
@@ -0,0 +1,17 @@
+import { IsNotEmpty } from 'class-validator';
+import { CategoryEntity } from '../../category/entities/category.entity';
+import { BaseDto } from '../../config/base.dto';
+
+export class ProductDto extends BaseDto {
+  @IsNotEmpty()
+  productName!: string;
+
+  @IsNotEmpty()
+  description!: string;
+
+  @IsNotEmpty()
+  price!: number;
+
+  @IsNotEmpty()
+  category!: CategoryEntity;
+}
