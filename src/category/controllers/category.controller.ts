ebf3f137c395281f1ab80f9cd85173f13d1890f9
Juan Lavallen <juanlavallen02@gmail.com>
2022-06-19 17:06:36 -0300

    Category Route added

--- src/category/controllers/category.controller.ts
+++ src/category/controllers/category.controller.ts
@@ -2,7 +2,9 @@ import { Request, Response } from 'express';
 import { CategoryService } from '../services/category.service';
 
 export class CategoryController {
-  constructor(private readonly categoryService: CategoryService) {}
+  constructor(
+    private readonly categoryService: CategoryService = new CategoryService()
+  ) {}
 
   async getCategories(req: Request, res: Response) {
     try {
