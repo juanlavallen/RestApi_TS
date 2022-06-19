65dc18770725b3995efe2b165f7981d46332fa4a
Juan Lavallen <juanlavallen02@gmail.com>
2022-06-19 13:20:06 -0300

    updateCategory controller added

--- src/category/controllers/category.controller.ts
+++ src/category/controllers/category.controller.ts
@@ -31,4 +31,14 @@ export class CategoryController {
       console.log(error);
     }
   }
+
+  async updateCategory(req: Request, res: Response) {
+    const { id } = req.params;
+    try {
+      const data = await this.categoryService.updateCategory(id, req.body);
+      res.status(200).json(data);
+    } catch (e) {
+      console.error(e);
+    }
+  }
 }
