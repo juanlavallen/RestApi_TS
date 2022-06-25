5e64af834811de169d27197e36cfd9ef19bbbeca
Juan Lavallen <juanlavallen02@gmail.com>
2022-06-24 23:04:11 -0300

    Product route added

---
+++ src/product/product.router.ts
@@ -0,0 +1,30 @@
+import { BaseRouter } from '../shared/router/router';
+import { ProductController } from './controllers/product.controller';
+
+export class ProductRouter extends BaseRouter<ProductController> {
+  constructor() {
+    super(ProductController);
+  }
+
+  routes(): void {
+    this.router.get('/products', (req, res) => {
+      this.controller.getProducts(req, res);
+    });
+
+    this.router.get('/product/:id', (req, res) => {
+      this.controller.getProductById(req, res);
+    });
+
+    this.router.post('/create', (req, res) => {
+      this.controller.createProduct(req, res);
+    });
+
+    this.router.put('/product/:id', (req, res) => {
+      this.controller.updateProduct(req, res);
+    });
+
+    this.router.delete('/product/:id', (req, res) => {
+      this.controller.deleteProduct(req, res);
+    });
+  }
+}
