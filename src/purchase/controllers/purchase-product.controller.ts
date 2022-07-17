f244fbc0607f75efbbba3cdb8da7ca2f979a0cd9
Juan Lavallen <juanlavallen02@gmail.com>
2022-07-17 14:52:12 -0300

    getPurchaseProducts finished

--- src/purchase/controllers/purchase-product.controller.ts
+++ src/purchase/controllers/purchase-product.controller.ts
@@ -7,4 +7,17 @@ export class PurchaseProductController {
     private readonly httpResponse: HttpResponse = new HttpResponse(),
     private readonly purchaseProductService: PurchaseProductService = new PurchaseProductService()
   ) {}
+
+  async getPurchaseProducts(req: Request, res: Response) {
+    try {
+      const data = await this.purchaseProductService.findAllPurchaseProducts();
+      if (data.length === 0) {
+        return this.httpResponse.NotFound(res, 'No se encontro la data');
+      }
+      return this.httpResponse.Ok(res, data);
+    } catch (error) {
+      console.log(error);
+      return this.httpResponse.Error(res, error);
+    }
+  }
 }
